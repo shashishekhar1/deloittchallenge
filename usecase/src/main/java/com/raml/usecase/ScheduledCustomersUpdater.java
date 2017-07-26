f3a0b97a1448ae9522671c7e0382e3a407f3bcdb
shashishekhar1 <shashishekhar1@gmail.com>
2017-07-27 09:43:16 +1000

    Update ScheduledCustomersUpdater.java

--- usecase/src/main/java/com/raml/usecase/ScheduledCustomersUpdater.java
+++ usecase/src/main/java/com/raml/usecase/ScheduledCustomersUpdater.java
@@ -3,7 +3,10 @@ package com.raml.usecase;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-
+/**
+*This calls will call CustomerApiExecutor every 5 minutes and update an
+*in memory record of customers.
+*/
 public class ScheduledCustomersUpdater {
 
 	public static void main(String[] args) {
